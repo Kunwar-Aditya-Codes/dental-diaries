1b5bd9922fafcdaa29394693a3aadf2ef7fdc814
Kunwar Aditya <adityakunwar0147@gmail.com>
2023-06-10 20:00:36 +0530

    added: tweets

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,6 +1,54 @@
 import { FC } from "react";
 import Hero from "/hero.png";
 import { Link } from "react-router-dom";
+import { AiOutlineTwitter } from "react-icons/ai";
+
+const tweets = [
+  {
+    id: 1,
+    tweet:
+      "Just had the most amazing experience at #DentalDiaries! The staff was incredibly friendly, and my teeth have never looked better! Thank you so much!",
+    name: "@SmilingSarah",
+  },
+
+  {
+    id: 2,
+    tweet:
+      "Shoutout to #DentalDiaries for making my dental visit stress-free and comfortable. The dentists are highly skilled, and the atmosphere is so relaxing. I'm thrilled with my new smile!",
+    name: "@SparklingSam",
+  },
+
+  {
+    id: 3,
+    tweet:
+      "I can't thank #DentalDiaries enough for the exceptional care I received today. From start to finish, the team was attentive and professional. My teeth are shining bright, and I couldn't be happier!",
+    name: "@JoyfulJulia",
+  },
+  {
+    id: 4,
+    tweet:
+      "Just had a fantastic check-up at #DentalDiaries. The hygienist was thorough, and the dentist provided great advice. I feel confident in their expertise, and my teeth are grateful!",
+    name: "@RadiantRobert",
+  },
+  {
+    id: 5,
+    tweet:
+      "Kudos to #DentalDiaries for the outstanding service. The staff was super friendly, and the clinic is equipped with top-notch technology. My dental issues were resolved with ease. Highly recommend! ",
+    name: "@EnergeticEmily",
+  },
+  {
+    id: 6,
+    tweet:
+      "A big thanks to #DentalDiaries for their gentle and caring approach. The dentist was compassionate and knowledgeable, ensuring my comfort throughout the procedure. Delighted with the results!",
+    name: "@DazzlingDave",
+  },
+  {
+    id: 7,
+    tweet:
+      "Just got my braces off at #DentalDiaries, and my smile is simply stunning! The orthodontist was fantastic, and the entire team made the process smooth. Grateful for their expertise!",
+    name: "@GlowingGrace",
+  },
+];
 
 const Home: FC = () => {
   return (
@@ -19,7 +67,7 @@ const Home: FC = () => {
             <Link
               to="/user_register"
               className="rounded-sm bg-secondary px-4 py-2 text-xl
-            font-medium tracking-wider text-primary drop-shadow-[3px_5px_30px_rgba(182,236,255,1)] transition ease-out hover:drop-shadow-[3px_5px_40px_rgba(182,236,255,1)]  lg:px-8 lg:py-4 lg:text-2xl"
+            font-medium tracking-wider text-primary drop-shadow-[3px_5px_30px_rgba(182,236,255,0.6)] transition ease-out hover:drop-shadow-[3px_5px_40px_rgba(182,236,255,1)]  lg:px-8 lg:py-4 lg:text-2xl"
             >
               Register
             </Link>
@@ -35,9 +83,25 @@ const Home: FC = () => {
       </div>
 
       {/* Tweets Card Section */}
-      <div className="w-full bg-white text-primary">
-        <div className="mx-auto max-w-[85rem] p-4">
-          <div></div>
+      <div className="mt-[8rem] w-full  text-secondary">
+        <h1 className="mx-2 mb-16 text-center font-custom text-2xl italic drop-shadow-[3px_5px_50px_rgba(182,236,255,1)] sm:text-4xl md:text-6xl">
+          Our customers love us...
+        </h1>
+        <div className="mx-auto grid max-w-[85rem] grid-cols-1 justify-items-center gap-[5rem] px-4 py-8 lg:grid-cols-2 ">
+          {tweets.map((tweet) => (
+            <div
+              key={tweet.id}
+              className="mx-2 flex max-h-[25rem] max-w-[35rem] cursor-default items-start  justify-between space-x-4 rounded-md bg-secondary/10 px-4 py-6 shadow-lg backdrop-blur-md"
+            >
+              <AiOutlineTwitter className="h-8 w-8 flex-[0.2]  text-sky-500" />
+              <div className="flex h-full flex-[0.8] flex-col justify-between px-4 lg:space-y-4 ">
+                <p className="text-justify text-base md:text-lg">
+                  {tweet.tweet}
+                </p>
+                <p className="text-right font-custom text-sm ">{tweet.name}</p>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
 
