8a59c038881d0e5094c2ec45a4d60a036b331be1
Kunwar Aditya <adityakunwar0147@gmail.com>
2023-06-12 06:23:51 +0530

    deploy: prepared files for deployment

--- server/config/db.js
+++ server/config/db.js
@@ -1,20 +1,36 @@
-const { Sequelize } = require('sequelize');
+const { Sequelize } = require("sequelize");
+
+const environment = process.env.NODE_ENV;
 
 /**
  * @desc: New Sequelize instance is created.
  *
  */
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    dialect: 'postgres',
-    port: process.env.DB_PORT,
-    dialectModule: require('pg'),
-    host: process.env.DB_HOST,
-  }
-);
+let sequelize;
+if (environment === "development") {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      dialect: "postgres",
+      port: process.env.DB_PORT,
+      dialectModule: require("pg"),
+      host: process.env.DB_HOST,
+    }
+  );
+} else {
+  sequelize = new Sequelize(process.env.POSTGRES_URL, {
+    dialectModule: require("pg"),
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  });
+}
 
 /**
  * @desc: Test the connection to the database.
@@ -22,9 +38,9 @@ const sequelize = new Sequelize(
 const testConnection = async () => {
   try {
     await sequelize.authenticate();
-    console.log('Connection has been established successfully.');
+    console.log("Connection has been established successfully.");
   } catch (err) {
-    console.error('Unable to connect to the database:', err.message);
+    console.error("Unable to connect to the database:", err.message);
   }
 };
 
--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ require("./config/associations");
 const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
+const path = require("path");
 
 const { sequelize, testConnection } = require("./config/db");
 
@@ -24,6 +25,15 @@ app.use(cookieParser());
 app.use("/api/auth", require("./routes/authRoute"));
 app.use("/api/admin", require("./routes/adminRoute"));
 app.use("/api/user", require("./routes/userRoute"));
+
+/**
+ * @desc: Serve static assets if in production.
+ */
+__dirname = path.resolve();
+app.use(express.static(path.join(__dirname, "client/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client/dist", "index.html"));
+});
 
 /**
  * @desc: Connect to the database and start the server.
